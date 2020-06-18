1b342af409b798d168afa6e8b7b024b7e4ba9704
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-06-18 09:22:14 -0300

    Added useVisib timer once

---
+++ 8-useVisibility/timer/2-left-right-once/App.js
@@ -0,0 +1,62 @@
+import React, {
+	createRef,
+	useEffect,
+	useState,
+  useCallback
+} from "react";
+import "./App.css";
+
+const useVisibilityTimeAlways = (id, offset) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [wasAlreadyVisible, setWasAlreadyVisible] = useState(false);
+  const currentElement = createRef(null);
+
+  const checkIsVisible = useCallback(top => (
+    top + offset >= 0 && top + offset <= window.innerHeight
+  ), [offset]);
+
+	useEffect(() => {
+    if (!wasAlreadyVisible) {
+      const intervalId = setInterval(() => {
+        setIsVisible(checkIsVisible(
+          currentElement.current?.getBoundingClientRect().top
+        ));
+      }, 100);
+      if (isVisible) setWasAlreadyVisible(true);
+      return () => clearInterval(intervalId);
+    }
+  }, [checkIsVisible, currentElement, isVisible, wasAlreadyVisible]);
+
+  return [isVisible, currentElement];
+};
+
+const App = () => {
+	const [isFirstVisible, firstRef] = useVisibilityTimeAlways(1, 100);
+	const [isSecondVisible, secondRef] = useVisibilityTimeAlways(2, 100);
+	const firstClassName = isFirstVisible 
+		? "first-element-on"
+		: "first-element-off";
+	const secondClassName = isSecondVisible 
+		? "second-element-on"
+		: "second-element-off";
+
+	useEffect(() => {
+		document.title = `${isFirstVisible} - ${isSecondVisible}`
+	}, [isFirstVisible, isSecondVisible]);
+
+	return (
+		<div className="app">
+			<div ref={firstRef} className={firstClassName}>
+				Event isFirstVisible
+			</div>
+			<div className="second-overflow">
+				<div ref={secondRef} className={secondClassName}>
+					Event isSecondVisible
+				</div>
+			</div>
+		</div>
+	);
+};
+
+
+export default App;
