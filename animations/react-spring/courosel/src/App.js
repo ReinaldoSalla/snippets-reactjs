0c986ee39659e6680a128125c9e71e2d9c809e66
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-07-06 08:36:03 -0300

    Added react-spring/courosel v3 (labels)

--- animations/react-spring/courosel/src/App.js
+++ animations/react-spring/courosel/src/App.js
@@ -1,13 +1,20 @@
 /*
-problems
+todo
 1-click the last one
-2-opacity fells strage at times
 3-why 0px and not 0% or just 0?
 4-user goes to another page and comes back seens to thigger the courosel
+5-relashion between timer and inputs, becase now it seens one breaks the other
+6-make the opacity fells right
+5-reducing the size of the buttons with flexbox insted of hardcoding media queies
 */
 
-import React, { useCallback, useEffect, Fragment, useReducer } from 'react';
-import { useTransition, animated } from 'react-spring';
+import React, {
+  useCallback,
+  useEffect,
+  Fragment,
+  useReducer
+} from 'react';
+import { useTransition, animated, useSpring } from 'react-spring';
 import './App.css';
 import js1 from './assets/js1.png';
 import js2 from './assets/js2.jpg';
@@ -19,18 +26,18 @@ const duration = 5000;
 
 const CouroselItem = ({ style, img }) => (
   <animated.div className='courosel-item' style={style}>
-    <img className='courosel-img' src={img} alt=''/>
+    <img className='courosel-img' src={img} alt='' />
   </animated.div>
 );
 
 const couroselImgs = [js1, js2, js3, js4, js5];
 
-const couroselItems = couroselImgs.map(item => 
+const couroselItems = couroselImgs.map(item =>
   ({ style }) => <CouroselItem style={style} img={item} />
 );
 
-const moveToNextItem = (state) => { 
-  let newIndex = state.index++;
+const moveToNextItem = (state) => {
+  let newIndex = state.index + 1;
   if (newIndex === 5) newIndex = 0;
   return { index: newIndex };
 };
@@ -52,7 +59,7 @@ const reducer = (state, action) => {
     case 'MOVE_TO_FIRST_ITEM':
       return moveToFirstItem();
     case 'MOVE_TO_SECOND_ITEM':
-      return moveToSecondItem(); 
+      return moveToSecondItem();
     case 'MOVE_TO_THIRD_ITEM':
       return moveToThirdItem();
     case 'MOVE_TO_FORTH_ITEM':
@@ -97,14 +104,28 @@ const App = () => {
     }, duration)
     return () => clearInterval(intervalId);
   }, [handleNextItem]);
-  
-  const transitions = useTransition(state.index, p => p,  {
+
+  const transitions = useTransition(state.index, p => p, {
     config: { mass: 2, tension: 70, friction: 24, precision: 0.001 },
     initial: { opacity: 1, transform: 'translate3d(0%, 0, 0)', },
     from: { opacity: 1, transform: 'translate3d(-100%,0,0)' },
     enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
     leave: { opacity: 1, transform: 'translate3d(0,0,0)' },
   });
+
+  const props = useSpring({
+    to: { background: 'white' }
+  });
+
+  const styleFirstInput = state.index === 0 ? props : null;
+
+  const styleSecondInput = state.index === 1 ? props : null;
+
+  const styleThirdInput = state.index === 2 ? props : null;
+
+  const styleForthInput = state.index === 3 ? props : null;
+
+  const styleFifthInput = state.index === 4 ? props : null;
 
   return (
     <Fragment>
@@ -119,11 +140,31 @@ const App = () => {
       </div>
       <div className='courosel-inputs-container'>
         <div className='courosel-inputs'>
-          <div className='courosel-input' onClick={handleFirstItem}  />
-          <div className='courosel-input' onClick={handleSecondItem} />
-          <div className='courosel-input' onClick={handleThirdItem}  />
-          <div className='courosel-input' onClick={handleForthItem}  />
-          <div className='courosel-input' onClick={handleFifthItem}  />
+          <animated.div 
+            style={styleFirstInput} 
+            className='courosel-input' 
+            onClick={handleFirstItem} 
+          />
+          <animated.div 
+            style={styleSecondInput}
+            className='courosel-input' 
+            onClick={handleSecondItem} 
+          />
+          <animated.div 
+            style={styleThirdInput}
+            className='courosel-input' 
+            onClick={handleThirdItem} 
+          />
+          <animated.div 
+            style={styleForthInput}
+            className='courosel-input' 
+            onClick={handleForthItem} 
+          />
+          <animated.div 
+            style={styleFifthInput}
+            className='courosel-input' 
+            onClick={handleFifthItem} 
+          />
         </div>
       </div>
     </Fragment>
