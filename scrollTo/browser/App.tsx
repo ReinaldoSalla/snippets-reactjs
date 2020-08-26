ba63e5cf7288250efacd04fcbbe1cad2dbfdf094
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-08-26 16:00:06 -0300

    Added scrollTop/browser with useCallback for passing locations between components

--- scrollTo/browser/App.tsx
+++ scrollTo/browser/App.tsx
@@ -1,47 +1,44 @@
-import React, { Fragment, useRef, useEffect } from 'react'
+import React, { Fragment, useRef, useEffect, useState, useCallback } from 'react'
 import styled from 'styled-components';
 
-const Button = styled.button`
+interface ButtonProps {
+  top: string;
+}
+
+const Button = styled.button<ButtonProps>`
   position: fixed;
-  top: 50px;
+  top: ${props => props.top};
   right: 50px;
 `;
 
-const Divs = ({ num }) => {
-  const firstRef: any = useRef(null);
-  const secondRef: any = useRef(null);
-  const thirdRef: any = useRef(null);
-  const forthRef: any = useRef(null);
-
-  useEffect(() => {
-    console.log(firstRef.current.offsetTop);
-    console.log(secondRef.current.offsetTop);
-    console.log(thirdRef.current.offsetTop);
-    console.log(forthRef.current.offsetTop);
-  }, []);
-
+const Divs = ({ 
+  firstRef, 
+  secondRef,
+  thirdRef,
+  forthRef 
+}) => {
   return (
     <Fragment>
       <h1 ref={firstRef}>first breakpoint</h1>
-      {new Array(num).fill(0).map((_, index) => 
+      {new Array(100).fill(0).map((_, index) => 
         <div key={index}>
           content #{index + 1}
         </div>
       )}
       <h1 ref={secondRef}>second breakpoint</h1>
-      {new Array(num).fill(0).map((_, index) => 
+      {new Array(100).fill(0).map((_, index) => 
         <div key={index}>
           content #{index + 1}
         </div>
       )}
       <h1 ref={thirdRef}>third breakpoint</h1>
-      {new Array(num).fill(0).map((_, index) => 
+      {new Array(100).fill(0).map((_, index) => 
         <div key={index}>
           content #{index + 1}
         </div>
       )}
       <h1 ref={forthRef}>forth breakpoint</h1>
-      {new Array(num).fill(0).map((_, index) => 
+      {new Array(100).fill(0).map((_, index) => 
         <div key={index}>
           content #{index + 1}
         </div>
@@ -51,15 +48,55 @@ const Divs = ({ num }) => {
 };
 
 export default function App() {
-  const onClick = () => {
-    window.scroll({ top: 3783, behavior: 'smooth' });
-  }
+  const [firstOffsetTop, setFirstOffsetTop] = useState(0);
+  const [secondOffsetTop, setSecondOffsetTop] = useState(0);
+  const [thirdOffsetTop, setThirdOffsetTop] = useState(0);
+  const [forthOffsetTop, setForthOffsetTop] = useState(0);
+
+  const firstRef = useCallback(node => {
+    if (node !== null) setFirstOffsetTop(node.offsetTop)
+  }, []);
+
+  const secondRef = useCallback(node => {
+    if (node !== null) setSecondOffsetTop(node.offsetTop)
+  }, []);
+
+  const thirdRef = useCallback(node => {
+    if (node !== null) setThirdOffsetTop(node.offsetTop)
+  }, []);
+
+  const forthRef = useCallback(node => {
+    if (node !== null) setForthOffsetTop(node.offsetTop);
+  }, []);
+
+  const onClickFirst = () => {
+    window.scroll({ top: firstOffsetTop, behavior: 'smooth' });
+  };
+
+  const onClickSecond = () => {
+    window.scroll({ top: secondOffsetTop, behavior: 'smooth' });
+  };
+
+  const onClickThird = () => {
+    window.scroll({ top: thirdOffsetTop, behavior: 'smooth' });
+  };
+
+  const onClickForth = () => {
+    window.scroll({ top: forthOffsetTop, behavior: 'smooth' });
+  };
+
   return (
     <Fragment>
-      <Button onClick={onClick}>
-        back to top
-      </Button>
-      <Divs num={100}/>
+      <Button top='50px' onClick={onClickFirst}>first breakpoint</Button>
+      <Button top='80px' onClick={onClickSecond}>second breakpoint</Button>
+      <Button top='110px' onClick={onClickThird}>third breakpoint</Button>
+      <Button top='140px' onClick={onClickForth}>forth breakpoint</Button>
+      <Divs 
+        firstRef={firstRef} 
+        secondRef={secondRef}
+        thirdRef={thirdRef}
+        forthRef={forthRef}
+      />
     </Fragment>
   );
 }
