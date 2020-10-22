bb9a5e3a98be938e71e8c9e84d51ea5a334ad836
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-10-22 10:26:17 -0300

    Added a new improved version of filling

--- animations/react-spring/filling/src/App.tsx
+++ animations/react-spring/filling/src/App.tsx
@@ -1,17 +1,47 @@
-import { useSpring, animated } from 'react-spring'
-import './App.css'
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { animated, useSpring } from 'react-spring';
 
-function App() {
-  const [open, toggle] = useState(false)
-  const props = useSpring({
-    width: open ? '0%' : '100%'
+const Wrapper = styled.div`
+  border: 1px solid black;
+  width: 500px;
+  height: 500px;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Filler = styled(animated.div)`
+  position: absolute;
+  background: red;
+  width: 100%;
+`;
+
+export default () => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleEnter = () => setIsHovering(true);
+
+  const handleLeave = () => setIsHovering(false);
+
+  const animation = useSpring({
+    config: isHovering
+      ? { mass: 1, tension: 170, friction: 26 }
+      : { mass: 2, tension: 170, friction: 26, clamp: true },
+    from: { height: '0%' },
+    to: async (next) => {
+      await next({ height: isHovering ?  '100%' : '0%' });
+    }
   });
-  return (
-    <div className="main" onClick={() => toggle(!open)}>
-      <animated.div className="fill" style={props} />
-    </div>
-  )
-}
 
-export default App;
+  return (
+    <Wrapper onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
+      <Filler style={animation} />
+    </Wrapper>
+  );
+};
+
